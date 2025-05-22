3a67cd82886ee8e500269c3cb7034d272a7a504c
Jürgen Ulshöfer <64464998+mmjulsho@users.noreply.github.com>
2025-05-22 09:28:03 +0200

    Add files via upload

---
+++ Thread_Bsp.java
@@ -0,0 +1,11 @@
+public class Thread_Bsp {
+
+    public Thread_Bsp() {
+        Zaehler t1 = new Zaehler ("Thread Nr. 1 zählt: " , 500);
+        Zaehler t2 = new Zaehler ("Thread Nr. 2 zählt: " , 500);
+        // startet die Threads, die dann ihre run-Methode ausführen
+        t1.start();
+        t2.start();        
+    }
+
+}
---
+++ Zaehler.java
@@ -0,0 +1,32 @@
+public class Zaehler extends Thread {
+
+    /* Beispiel für Threads:
+     Ein Zaehler mit Namen name zählt von 1 bis 10,
+     zwischen jedem Zählschritt wartet er eine bestimmte
+     wartezeit (in ms) und gibt dann den Stand des zaehlers
+     auf dem Bildschirm aus. */
+
+    private String name;
+    private int zaehler;
+    private int wartezeit;
+
+    //Konstruktor
+    public Zaehler (String n , int w) {
+        name = n;
+        zaehler = 0;
+        wartezeit = w;
+    }
+
+    // Methode run() überschreibt die gleichnamige Methode der Oberklasse.
+    public void run() {
+        try { // try und catch wegen sleep() nötig
+            for (int i = 1 ; i <= 10 ; i++) {
+                zaehler = i;
+                sleep(wartezeit);
+                System.out.println("Stand von " + name + " : " + zaehler);
+            }
+            System.out.println("Endstand von " + name + " : " + zaehler);
+        } catch (InterruptedException e) { }
+    }
+
+}
